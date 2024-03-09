19e252c0e21feb6a3fcfca1248af88a25cb231a9
Shreyas <shreyas.poyarekar@cymmetri.com>
2024-03-09 10:05:11 +0530

    host changed from 127.0.0.1. to 0.0.0.0

--- dev.py
+++ dev.py
@@ -275,4 +275,4 @@ async def get_mapped(data: dict):
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
