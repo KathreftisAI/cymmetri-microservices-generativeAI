1aaa1651d40d0e4124b3fc1cb21e9eee710e59e6
Shreyas <shreyas.poyarekar@cymmetri.com>
2024-04-24 18:18:43 +0530

    updated files for changes

--- database/connection.py
+++ database/connection.py
@@ -1,20 +1,60 @@
+# import os
+# from pymongo import MongoClient
+# from config.loader import Configuration
+# import platform
+# from fastapi import FastAPI, Form, Request, HTTPException, Header
+
+
+# def load_configuration():
+#     c = Configuration()
+#     return c.getConfiguration()
+
+# data = load_configuration()
+
+# MONGO_DETAILS = data["MONGODB_CONNECTION_STRING"]
+# BASE_TENANT_STRING = data["CYMMETRI_DB_PREFIX"] + "%s"
+# mongo_client = MongoClient(MONGO_DETAILS)
+
+# def get_collection(tenant_name: str, collection_name: str):
+#     try:
+#         mongo_tenant_details = BASE_TENANT_STRING % tenant_name
+#         database = mongo_client[mongo_tenant_details]
+#         generic_collection = database[collection_name]
+#         return generic_collection
+#     except Exception as e:
+#         raise HTTPException(status_code=400, detail="MONGODB_CONNECTION_ERROR")
+    
+
+# def get_master_collection(collection_name: str):
+#     try:
+#         database = mongo_client['cymmetri']
+#         master_collection = database[collection_name]
+#         return master_collection
+#     except Exception as e:
+#         raise HTTPException(status_code=400, detail=str(e))  # Providing exception details for better debugging
+        
+
 import os
 from pymongo import MongoClient
 from config.loader import Configuration
 import platform
+import logging
 from fastapi import FastAPI, Form, Request, HTTPException, Header
-
-
+ 
 def load_configuration():
-    c = Configuration()
+    if platform.system() == 'Windows':
+        c = Configuration(r"config.yaml")
+    else:
+        c = Configuration("/config/config.yaml")
+
     return c.getConfiguration()
-
+ 
 data = load_configuration()
-
+ 
 MONGO_DETAILS = data["MONGODB_CONNECTION_STRING"]
 BASE_TENANT_STRING = data["CYMMETRI_DB_PREFIX"] + "%s"
 mongo_client = MongoClient(MONGO_DETAILS)
-
+ 
 def get_collection(tenant_name: str, collection_name: str):
     try:
         mongo_tenant_details = BASE_TENANT_STRING % tenant_name
@@ -23,7 +63,6 @@ def get_collection(tenant_name: str, collection_name: str):
         return generic_collection
     except Exception as e:
         raise HTTPException(status_code=400, detail="MONGODB_CONNECTION_ERROR")
-    
 
 def get_master_collection(collection_name: str):
     try:
@@ -32,4 +71,3 @@ def get_master_collection(collection_name: str):
         return master_collection
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))  # Providing exception details for better debugging
-        
