84ece1778dce2c3d1ef8a40e561e6e2780f95748
Shreyas <shreyas.poyarekar@cymmetri.com>
2024-04-24 16:00:43 +0530

    updated the file path for config file

--- config/loader.py
+++ config/loader.py
@@ -8,7 +8,7 @@ class Configuration:
     def __init__(self, filepath=None):
         self.data = {}
         if filepath is None:
-            filepath = os.environ.get('CONFIG_FILE_PATH', 'config.yaml')
+            filepath = os.environ.get('CONFIG_FILE_PATH', 'config/config.yaml')
         with open(filepath, "r") as yamlfile:
             data_loaded = yaml.safe_load(yamlfile)
             self.data = data_loaded
